4de10b32568c93748546ee3a29b7312a52af3240
ANKAN BERA <beraankan8@gmail.com>
2025-06-02 01:41:01 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/types.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+    int msgid,len;
+    key_t key;
+    key = 131;
+    msgid = msgget(key, IPC_CREAT|0666);
+    printf("\nq=%d",msgid);
+} 	
